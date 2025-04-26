b68598d6532ef7f2b70dffb7eba925d8e08ea529
AngRod <sthefanyangeles00@gmail.com>
2025-04-26 11:34:34 -0400

    feat: Add health route for waking up the server

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import conectDB from './config/db.js';
-import worldmap from './routes/worldmap.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import conectDB from "./config/db.js";
+import worldmap from "./routes/worldmap.js";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-dotenv.config()
+dotenv.config();
 conectDB();
 
-
-
-//cors
-// const domainsAllowed = [process.env.FRONTEND_URL]
-
-// const corsConfig = {
-//     origin: function(origin, callback){
-//         if(domainsAllowed.indexOf(origin) !== -1){
-//             callback(null, true)
-//         }
-//         else{
-//             callback(new Error("Access Blocked"))
-//         }
-//     }
-// }
-
-app.use(cors({
-    origin: process.env.FRONTEND_URL, 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-}));
+  })
+);
 
-app.use('/worldmap', worldmap)
+app.use("/worldmap", worldmap);
 
-
-app.listen(PORT, ()=> {
-    console.log(`Servidor ejecutandose en ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutandose en ${PORT}`);
+});
--- routes/worldmap.js
+++ routes/worldmap.js
@@ -1,39 +1,68 @@
-import express from 'express'
-import multer from 'multer';
-import { register, authenticate, confirm, profile, updateProfile, sendEmailResetPassword, newPassword, changePassword } from '../controllers/UserController.js';
-import { saveCountry, getCountrys, deleteCountry, updateCountry } from '../controllers/MapController.js';
-import { saveNote, getNotes, deleteNote, updateNote, filterNote } from '../controllers/NoteController.js';
-import checkAuth from '../middleware/authMiddleware.js';
+import express from "express";
+import multer from "multer";
+import {
+  register,
+  authenticate,
+  confirm,
+  profile,
+  updateProfile,
+  sendEmailResetPassword,
+  newPassword,
+  changePassword,
+} from "../controllers/UserController.js";
+import {
+  saveCountry,
+  getCountrys,
+  deleteCountry,
+  updateCountry,
+} from "../controllers/MapController.js";
+import {
+  saveNote,
+  getNotes,
+  deleteNote,
+  updateNote,
+  filterNote,
+} from "../controllers/NoteController.js";
+import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 /*Configure Multer*/
 const storage = multer.memoryStorage();
-const upload = multer({storage})
+const upload = multer({ storage });
+
+/*Health Route*/
+router.get("/health", (_, res) => {
+  return res.status(200).send("OK");
+});
 
 /*User Routes*/
-router.post("/", register)
-router.post("/login", authenticate)
-router.get("/confirm/:token", confirm)
-router.get("/profile", checkAuth, profile)
-router.post("/profile/:id", upload.single('avatar'), checkAuth, updateProfile)
-router.post("/resetPassword", sendEmailResetPassword)
-router.post("/resetPassword/:token", newPassword)
-router.post("/changePassword", checkAuth, changePassword)
+router.post("/", register);
+router.post("/login", authenticate);
+router.get("/confirm/:token", confirm);
+router.get("/profile", checkAuth, profile);
+router.post("/profile/:id", upload.single("avatar"), checkAuth, updateProfile);
+router.post("/resetPassword", sendEmailResetPassword);
+router.post("/resetPassword/:token", newPassword);
+router.post("/changePassword", checkAuth, changePassword);
 
 /*Country Routes*/
-router.route("/country").post(checkAuth, saveCountry).get(checkAuth, getCountrys)
-router.route("/country/:id")
-    .delete(checkAuth ,deleteCountry)
-    .put(checkAuth, updateCountry)    
+router
+  .route("/country")
+  .post(checkAuth, saveCountry)
+  .get(checkAuth, getCountrys);
+router
+  .route("/country/:id")
+  .delete(checkAuth, deleteCountry)
+  .put(checkAuth, updateCountry);
 
 /*Notes Routes*/
-router.route("/notes").post(checkAuth, saveNote).get(checkAuth, getNotes)
-router.route("/notes/:id")
-    .delete(checkAuth ,deleteNote)
-    .put(checkAuth, updateNote)
+router.route("/notes").post(checkAuth, saveNote).get(checkAuth, getNotes);
+router
+  .route("/notes/:id")
+  .delete(checkAuth, deleteNote)
+  .put(checkAuth, updateNote);
 
-
-router.post("/notes/filter", checkAuth, filterNote)
+router.post("/notes/filter", checkAuth, filterNote);
 
 export default router;
